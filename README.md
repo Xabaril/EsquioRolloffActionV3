1de4c4bc78adf9fd8bef429577906ed0c47da8ca
Luis Fraile <lfraile@lfraile.net>
2020-04-11 11:46:35 +0200

    First version of rolloff action for v3

---
+++ README.md
@@ -0,0 +1,25 @@
+# Esquio v3.0 Github Action rolloff feature
+
+With this Esquio Github action you can disable a feature in an Github Actions workflow.[Esquio](https://esquio.readthedocs.io/en/latest/).
+
+Please read [Esquio readthedocs](https://esquio.readthedocs.io/en/latest/) first to fully understand Esquio Feature Toggle package configuration and possibilities.
+
+## Parameters needed
+
+- **esquioUrl**: Url to the Esquio Api. i.e.: https://myesquioui.deployment.com
+- **esquioApiKey**: API key to authenticate to esquio. Recommended to store as [Github secret](https://help.github.com/en/github/automating-your-workflow-with-github-actions/virtual-environments-for-github-actions#creating-and-using-secrets-encrypted-variables)
+- **productName**: Name of the product to which the feature belongs.
+- **featureName**: Name of the feature to disable.
+
+## Example
+
+```YAML
+      - name: Esquio rolloff
+        uses: actions/esquio-rolloff-v3
+        id: esquio-rolloff-v3
+        with:
+          esquioUrl: 'https://esquiodemoui.azurewebsites.net/'
+          esquioApiKey: ${{ secrets.apikey }}
+          productName: 'Default'
+          featureName: 'MatchScore'
+```
---
+++ src/main.ts
@@ -0,0 +1,49 @@
+import * as core from '@actions/core';
+import url =  require('url'); 
+const https = require('https');
+
+async function run() {
+  try {
+    const esquioUrl = core.getInput('esquioUrl');
+    const esquioApiKey = core.getInput('esquioApiKey');
+    const productName = core.getInput('productName');
+    const featureName = core.getInput('featureName');
+
+    await rollOffFeature(url.parse(esquioUrl), esquioApiKey, productName, featureName);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function rollOffFeature(esquioUrl: url.UrlWithStringQuery, esquioApiKey: string, productName: string, featureName: string) {
+  const options = {
+      hostname: esquioUrl.host,
+      path: `/api/products/${productName}/features/${featureName}/rollback`,
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': esquioApiKey,
+          'x-api-version': '3.0'
+      }
+  }
+  console.log(`url to call ${esquioUrl.host} ${options.path}`);
+  const req = https.request(options, (res: any) => {
+      if (res.statusCode === 200) {
+          console.log('Feature rolloff succesful');
+      }
+
+      res.on('data', (data: any) => {
+          if (res.statusCode != 200) {
+              const responseData = JSON.parse(data);
+              core.setFailed(`Error in feature rolloff ${responseData.detail} HttpCode: ${res.statusCode}`);
+          }
+      });
+  });
+  req.on('error', (error: any) => {
+    core.setFailed(error);
+  });
+
+  req.end();
+}
+
+run();
